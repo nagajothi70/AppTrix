f8250336749676b89e9a9a6b2ebe5572e463dce1
nagajothi70 <nagajothierode7@gmail.com>
2026-04-19 18:44:29 +0530

    completed login signin firebase setup.

--- app/src/main/java/com/example/apptrix/ui/authentication/LoginScreen.kt
+++ app/src/main/java/com/example/apptrix/ui/authentication/LoginScreen.kt
@@ -1,44 +1,36 @@
 package com.example.apptrix.ui.authentication
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.material3.ExposedDropdownMenuDefaults.textFieldColors
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.models.sealed.Screen
+import com.google.firebase.auth.FirebaseAuth
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavController) {
+
+    val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
+    var passwordVisible by remember { mutableStateOf(false) }
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
@@ -59,52 +51,47 @@ fun LoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Center
         ) {
 
-            Text(
-                text = "AppTrix",
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
+            Text("AppTrix", fontSize = 32.sp, fontWeight = FontWeight.Bold, color = Color.White)
 
-            Text(
-                text = "Welcome Back 👋",
-                color = Color.LightGray,
-                fontSize = 14.sp
-            )
+            Text("Welcome Back 👋", color = Color.LightGray, fontSize = 14.sp)
 
             Spacer(modifier = Modifier.height(40.dp))
 
-            // EMAIL FIELD
             TextField(
                 value = email,
                 onValueChange = { email = it },
                 placeholder = { Text("Email / Phone", color = Color.LightGray) },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    focusedIndicatorColor = Color.White,
-                    unfocusedIndicatorColor = Color.Gray,
-                    cursorColor = Color.White
-                ),
-                textStyle = LocalTextStyle.current.copy(color = Color.White),
+                colors = textFieldColors(),
                 modifier = Modifier.fillMaxWidth()
             )
 
             Spacer(modifier = Modifier.height(20.dp))
 
-            // PASSWORD FIELD
             TextField(
                 value = password,
                 onValueChange = { password = it },
                 placeholder = { Text("Password", color = Color.LightGray) },
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    focusedIndicatorColor = Color.White,
-                    unfocusedIndicatorColor = Color.Gray,
-                    cursorColor = Color.White
-                ),
-                textStyle = LocalTextStyle.current.copy(color = Color.White),
+
+                // 👇 main logic
+                visualTransformation = if (passwordVisible)
+                    VisualTransformation.None
+                else
+                    PasswordVisualTransformation(),
+
+                trailingIcon = {
+                    val icon = if (passwordVisible)
+                        Icons.Default.Visibility
+                    else
+                        Icons.Default.VisibilityOff
+
+                    IconButton(onClick = {
+                        passwordVisible = !passwordVisible
+                    }) {
+                        Icon(imageVector = icon, contentDescription = null)
+                    }
+                },
+
+                colors = textFieldColors(),
                 modifier = Modifier.fillMaxWidth()
             )
 
@@ -112,14 +99,31 @@ fun LoginScreen(navController: NavController) {
 
             Button(
                 onClick = {
-                    navController.navigate(Screen.Home.route)
+
+                    if (email.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(context, "Fill all fields", Toast.LENGTH_SHORT).show()
+                        return@Button
+                    }
+
+                    auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                Toast.makeText(context, "Login Success", Toast.LENGTH_SHORT).show()
+
+                                navController.navigate(Screen.Home.route) {
+                                    popUpTo(Screen.Login.route) { inclusive = true }
+                                }
+
+                            } else {
+                                Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
                 },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
+                modifier = Modifier.fillMaxWidth().height(50.dp),
                 shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF4A90E2)
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4A90E2)
                 )
             ) {
                 Text("Login", color = Color.White)
@@ -130,13 +134,10 @@ fun LoginScreen(navController: NavController) {
             Row {
                 Text("Don't have an account?", color = Color.LightGray)
                 Spacer(modifier = Modifier.width(5.dp))
-                Text(
-                    "Sign Up",
-                    color = Color.White,
+                Text("Sign Up", color = Color.White,
                     modifier = Modifier.clickable {
                         navController.navigate(Screen.Signup.route)
-                    }
-                )
+                    })
             }
         }
     }
--- app/src/main/java/com/example/apptrix/ui/authentication/SignupScreen.kt
+++ app/src/main/java/com/example/apptrix/ui/authentication/SignupScreen.kt
@@ -1,15 +1,18 @@
 package com.example.apptrix.ui.authentication
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
+import androidx.compose.material3.ExposedDropdownMenuDefaults.textFieldColors
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -17,9 +20,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.models.sealed.Screen
+import com.google.firebase.auth.FirebaseAuth
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignupScreen(navController: NavController) {
+
+    val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
 
     var username by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
@@ -42,74 +50,67 @@ fun SignupScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize()
         ) {
 
-            Text(
-                "Create Account",
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
+            Text("Create Account", fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.White)
 
             Spacer(modifier = Modifier.height(30.dp))
 
-            // Username
-            TextField(
-                value = username,
-                onValueChange = { username = it },
+            TextField(value = username, onValueChange = { username = it },
                 placeholder = { Text("Username", color = Color.LightGray) },
-                colors = textFieldColors(),
-                modifier = Modifier.fillMaxWidth()
-            )
+                colors = textFieldColors(), modifier = Modifier.fillMaxWidth())
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Email
-            TextField(
-                value = email,
-                onValueChange = { email = it },
+            TextField(value = email, onValueChange = { email = it },
                 placeholder = { Text("Email", color = Color.LightGray) },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                colors = textFieldColors(),
-                modifier = Modifier.fillMaxWidth()
-            )
+                colors = textFieldColors(), modifier = Modifier.fillMaxWidth())
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Phone
-            TextField(
-                value = phone,
-                onValueChange = { phone = it },
+            TextField(value = phone, onValueChange = { phone = it },
                 placeholder = { Text("Mobile Number", color = Color.LightGray) },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                colors = textFieldColors(),
-                modifier = Modifier.fillMaxWidth()
-            )
+                colors = textFieldColors(), modifier = Modifier.fillMaxWidth())
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Password
-            TextField(
-                value = password,
-                onValueChange = { password = it },
+            TextField(value = password, onValueChange = { password = it },
                 placeholder = { Text("Password", color = Color.LightGray) },
                 visualTransformation = PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                colors = textFieldColors(),
-                modifier = Modifier.fillMaxWidth()
-            )
+                colors = textFieldColors(), modifier = Modifier.fillMaxWidth())
 
             Spacer(modifier = Modifier.height(20.dp))
 
             Button(
                 onClick = {
-                    navController.navigate(Screen.Home.route)
+
+                    if (email.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(context, "Fill all fields", Toast.LENGTH_SHORT).show()
+                        return@Button
+                    }
+
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                Toast.makeText(context, "Signup Success", Toast.LENGTH_SHORT).show()
+
+                                navController.navigate(Screen.Home.route)
+
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    task.exception?.message ?: "Error",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+
                 },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
+                modifier = Modifier.fillMaxWidth().height(50.dp),
                 shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF4A90E2)
-                )
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4A90E2))
             ) {
                 Text("Sign Up", color = Color.White)
             }
@@ -119,23 +120,11 @@ fun SignupScreen(navController: NavController) {
             Row {
                 Text("Already have an account?", color = Color.LightGray)
                 Spacer(modifier = Modifier.width(5.dp))
-                Text(
-                    "Login",
-                    color = Color.White,
+                Text("Login", color = Color.White,
                     modifier = Modifier.clickable {
                         navController.navigate(Screen.Login.route)
-                    }
-                )
+                    })
             }
         }
     }
 }
-
-@Composable
-fun textFieldColors() = TextFieldDefaults.colors(
-    focusedContainerColor = Color.Transparent,
-    unfocusedContainerColor = Color.Transparent,
-    focusedIndicatorColor = Color.White,
-    unfocusedIndicatorColor = Color.Gray,
-    cursorColor = Color.White
-)
