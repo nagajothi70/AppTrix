165fce6ef42d965a3486041f7426286247f11437
nagajothi70 <nagajothierode7@gmail.com>
2026-04-25 23:49:00 +0530

    fixed 1 month authentication logic to  1 week

--- app/src/main/java/com/example/apptrix/AppNav.kt
+++ app/src/main/java/com/example/apptrix/AppNav.kt
@@ -1,21 +1,10 @@
 package com.example.apptrix
 
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.biometric.BiometricPrompt
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
 import androidx.navigation.compose.*
 import com.example.apptrix.ui.HomeScreen
 import com.example.apptrix.ui.authentication.AuthLoadingScreen
-import com.example.apptrix.ui.authentication.AuthLoadingView
 import com.example.apptrix.ui.authentication.BiometricScreen
 import com.example.apptrix.ui.authentication.ForgotPasswordScreen
 import com.example.apptrix.ui.authentication.LoginScreen
@@ -38,9 +27,8 @@ fun AppNav() {
     val diff = currentTime - savedTime
     val days = diff / (1000 * 60 * 60 * 24)
 
-    // 🔥 Dynamic start destination
     val startDestination = when {
-        savedTime == 0L || days >= 30 -> Screen.Login.route
+        savedTime == 0L || days >= 7 -> Screen.Login.route
         else -> Screen.Biometric.route
     }
 
